bbb8797c05926fd1819eca087c9fcafcf186ca2b
Rori Kotch <r.kotch1@umiami.edu>
2017-04-20 19:09:42 -0400

    Trying to show/hide mouse

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function(){
-  
-}
+/*  $("svg").click(function() {
+  if ( $('#cheese').css('visibility') == 'hidden' )
+    $('#cheese').css('visibility','visible');
+  else
+    $('#svg_2').css('visibility','hidden');
+});
+*/
