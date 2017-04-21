a71b6870215c91b7ab47f4cbd31d3bf5eb6f3f29
Rori Kotch <r.kotch1@umiami.edu>
2017-04-20 23:35:26 -0400

    Added more html

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-/*  $("svg").click(function() {
-  if ( $('#cheese').css('visibility') == 'hidden' )
-    $('#cheese').css('visibility','visible');
-  else
-    $('#svg_2').css('visibility','hidden');
+  /*var cheese = document.querySelector("#" + id);
+
+  // Now that we have the province the user has interacted with, let's
+  // store that node in a variable to work with further.
+
+  if (cheese.hasAttribute("class") == false) {
+    cheese.setAttribute("class", "selected");
+  } else {
+    cheese.removeAttribute("class");
+  }
+
+  // The above if statement creates a simple toggle effect. If there is
+  // no class on the element clicked, apply the class selected (we've
+  // created the style in .css). If it has the class attribute (and is
+  // therefore selected) and is clicked again, remove the attribute.
+
 });
 */
+
+var cheese;
+
+if (cheese.visible("class") == false) {
+  console.log("working");
+} else {
+  cheese.visibl("class")== true;
+}
