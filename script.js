1053fb874b78c8237dab5c00fba9bf84d66c2381
Rori Kotch <r.kotch1@umiami.edu>
2017-04-22 11:18:12 -0400

    Added variables to script
    Deleted hover notes

--- script.js
+++ script.js
@@ -1,37 +1,13 @@
-  /*var cheese = document.querySelector("#" + id);
-
-  // Now that we have the province the user has interacted with, let's
-  // store that node in a variable to work with further.
-
-  if (cheese.hasAttribute("class") == false) {
-    cheese.setAttribute("class", "selected");
-  } else {
-    cheese.removeAttribute("class");
-  }
-
-  // The above if statement creates a simple toggle effect. If there is
-  // no class on the element clicked, apply the class selected (we've
-  // created the style in .css). If it has the class attribute (and is
-  // therefore selected) and is clicked again, remove the attribute.
-
-});
-*/
-/*
-var cheese;
-
-if (cheese.visible("class") == false) {
-  console.log("working");
-} else {
-  cheese.visibl("class")== true;
-}
-*/
-
- /*
-var cheese;
-
-if (cheese.variable("cheese" == true) {
-  display.visible
-} else {
-
-}
-*/
+var blue = ["Chianti Classico", "Syrah"]
+var cheddar = ["Syrah", "Red Bordeaux", "Pinot Noir"]
+var epoisses = ["Alsatian Riesling", "Pinot Noir"]
+var gruyere = ["Syrah", "Tuscan Sangiovese"]
+var manchego = ["Pinot Noir"]
+var parm = ["California Merlot", "Barolo", "Parmigiano-Reggiano"]
+var merlot = ["Parmigiano-Reggiano"]
+var pinot-noir = ["Epoisses de Bourgogne", "Cheddar", "Manchego"]
+var red-bordeaux = ["Cheddar"]
+var syrah = ["Cheddar"]
+var chianti = ["Classic Blue"]
+var sangiovese = ["Gruyere"]
+var barolo = ["Cheddar", "Parmigiano-Reggiano"]
