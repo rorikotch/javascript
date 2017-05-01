15b7b12d9426b6b87c0d4262b1f5356899490c07
Rori Kotch <r.kotch1@umiami.edu>
2017-05-01 12:49:31 -0400

    Took out all the modal boxes
    AHHHHHHHHH

--- script4.js
+++ script4.js
@@ -1,70 +1,13 @@
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
-btn.onclick = function () {
-  modal.style.display = "block";
-}
-
-span.onclick = function () {
-  modal.style.display = "none";
-}
-
-// window.onclick = function (event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-
-
-
-
-
 cheddarsvg = document.getElementById("cheddar")
 cheddarsvg.addEventListener("click",function(){
   console.log(cheeses["cheddar"].wines);
 })
 
-// cheddarsvg.click(function(){
-//   // if (event.target == cheddarsvg)
-//     modal.style.display(cheeses["cheddar"].wines);
-// })
-
-document.getElementById("cheddarbutton")
-  "cheddarbutton".addEventListener("click", function() {
-    modal.display ="block";
-  })
-// btn.onclick = function() {
-//     modal.style.display = "block"
-//   }
-
-// window.onclick = function (event) {
-//   if (event.target == btn) {
-//     modal.style.display(cheeses["cheddar"].wines);
-//   }
-
-// cheddarsvg.addEventListener("click" , function(){
-//   window.alert(cheeses["cheddar"].wines);
-// });
 
 classicbluesvg = document.getElementById("classicblue")
 classicbluesvg.addEventListener("click",function(){
   console.log(cheeses["classicblue"].wines)
 })
-
-document.getElementById("classicbluebutton")
-  "classicbluebutton".addEventListener("click", function() {
-    modal.display ="block";
-  })
-
-// classicbluesvg.click(function(){
-//   if (event.target === classicbluesvg)
-//     modal.display(cheeses["classicblue"].wines);
-// })
-
-
-
-// classicbluesvg.addEventListener("click" , function(){
-//   window.alert(cheeses["classicblue"].wines);
-// });
 
 
 epoissesdesvg = document.getElementById("epoissesde")
@@ -73,43 +16,16 @@ epoissesdesvg.addEventListener("click",function(){
 })
 
 
-// epoissesdesvg.click(function(){
-//   if (event.target == epoissesdesvg)
-//     modal.style.display(cheeses["epoissesde"].wines);
-// })
-
-
-// epoissesdesvg.addEventListener("click" , function(){
-//   window.alert(cheeses["epoissesde"].wines);
-// });
 
 gruyeresvg = document.getElementById("gruyere")
 gruyeresvg.addEventListener("click",function(){
   console.log(cheeses["gruyere"].wines)
 })
 
-// gruyere.click(function(){
-//   if (event.target == gruyeresvg)
-//     modal.style.display(cheeses["gruyere"].wines);
-// })
-
-// gruyeresvg.addEventListener("click" , function(){
-//   window.alert(cheeses["gruyere"].wines);
-// });
-
 manchegosvg = document.getElementById("manchego")
 manchegosvg.addEventListener("click",function(){
   console.log(cheeses["manchego"].wines)
 })
-
-// manchegosvg.click(function(){
-//   if (event.target == manchegosvg)
-//     modal.style.display(cheeses["manchego"].wines);
-// })
-
-// manchegosvg.addEventListener("click" , function(){
-//   window.alert(cheeses["manchego"].wines);
-// });
 
 parmigianoreggianoSvg = document.getElementById("parmigianoreggiano")
 parmigianoreggianoSvg.addEventListener("click",function(){
